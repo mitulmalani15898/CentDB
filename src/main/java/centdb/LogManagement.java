b138fcf1ecabae0427e2c307d15c9bac65eedbc6
Fenil Deavngbhai Shah <fn290319@dal.ca>
2021-12-08 00:30:41 -0400

    Added Logic in Transaction and Logs

--- src/main/java/centdb/LogManagement.java
+++ src/main/java/centdb/LogManagement.java
@@ -1,6 +1,9 @@
 package centdb;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -27,20 +30,52 @@ public class LogManagement {
 		return executionTime;
 	}
 	
-	public static void generalLogs(String log, long time, String user) throws IOException {
+	public static String stateDatabase(String databaseName) {
+		String state = "";
+		String databasePath = "./"+databaseName+"/";
+		File[] allFiles = new File(databasePath).listFiles();
+		state=state+"Tables:"+allFiles.length;
+		int records=0;
+		for(int i=0;i<allFiles.length;i++) {
+			BufferedReader readFile;
+			try {
+				readFile = new BufferedReader(new FileReader(allFiles[i]));
+				while(readFile.readLine() != null) {
+					records++;
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		records=records-allFiles.length;
+		
+		state=state+"; Number of Records:"+records;
+
+		return state;
+	}
+	
+	//Call generalLogs for only Valid queries because "execution time" is asked
+	public static void generalLogs(String log, long time, String user, String databaseName) throws IOException {
 		general = new File(generalLogsFile);
 			if(general.createNewFile()) {
 				System.out.println("File for General Logs has been created");
 			}
 			writeGeneral = new FileWriter(general,true);
+			
+			String state = stateDatabase(databaseName);
+			
 			String executionTime = Long.toString(time);
-			writeGeneral.append(executionTime+" --> ");
+			writeGeneral.append(executionTime+" milliseconds --> ");
 			writeGeneral.append(user+" --> ");
+			writeGeneral.append(databaseName+" --> ");
+			writeGeneral.append(state+" --> ");
 			writeGeneral.append(log+"\r\n");
 			writeGeneral.flush();		
 	}
 	
-	public static void eventLogs(String log, String user) throws IOException {
+	//Call eventLogs for only Valid Queries
+	public static void eventLogs(String log, String user, String databaseName) throws IOException {
 		event = new File(eventLogsFile);
 			if(event.createNewFile()) {
 				System.out.println("File for Event Logs has been created");
@@ -51,11 +86,13 @@ public class LogManagement {
 			timestamp = formatDate.format(date);	
 			writeEvent.append(timestamp+" --> ");
 			writeEvent.append(user+" --> ");
+			writeEvent.append(databaseName+" --> ");
 			writeEvent.append(log+"\r\n");
 			writeEvent.flush();			
 	}
 	
-	public static void queryLogs(String log, String user) throws IOException {
+	//Call queryLogs method for every query-Valid & Invalid
+	public static void queryLogs(String log, String user, String databaseName) throws IOException {
 //		user = "User1";
 		query = new File(queryLogsFile);
 			if(query.createNewFile()) {
@@ -67,6 +104,7 @@ public class LogManagement {
 			timestamp = formatDate.format(date);
 			writeQuery.append(timestamp+" --> ");
 			writeQuery.append(user+" --> ");
+			writeQuery.append(databaseName+" --> ");
 			writeQuery.append(log+"\r\n");
 			writeQuery.flush();
 	}
--- src/main/java/centdb/TransactionProcessing.java
+++ src/main/java/centdb/TransactionProcessing.java
@@ -1,12 +1,17 @@
 package centdb;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TransactionProcessing {
 	public static boolean isTransactionQuery = false;
 	public static String transactionQuery="";
+//	public static ArrayList<String> queries = new ArrayList<String>();
 	
 //	public static void queryIdentify(String query) {
 	public static void main(String[] args) {
-		String originalQuery = "SELECT * FROM Person BEGIN TRANSACTION INSERT INTO Person VALUES('Mouse', 'Micky','500 South Buena Vista Street, Burbank','California',43) SAVE TRANSACTION InsertStatement DELETE Person WHERE PersonID=3 SELECT * FROM Person COMMIT SELECT * FROM Person";
+		String originalQuery = "SELECT * FROM Person; BEGIN TRANSACTION SELECT * FROM Person; INSERT INTO table(id, name, email) VALUES (11, 'def', 'ghi'); INSERT INTO Person VALUES('Mouse', 'Micky','500 South Buena Vista Street, Burbank','California',43); DELETE from Person WHERE PersonID=3; SELECT * FROM Person; COMMIT SELECT * FROM Person;";
 		String query = originalQuery.toLowerCase();
 		if(query.contains("begin transaction") || query.contains("begin tran")) {
 			if(query.contains("commit") || query.contains("rollback")) {
@@ -21,6 +26,8 @@ public class TransactionProcessing {
 					}
 				}
 				System.out.println(index);
+				//queryLogs(queryParts[index]+" "+queryParts[index+1])
+				index=index+2;
 				while(index < queryParts.length) {
 					if(! ( queryParts[index].equalsIgnoreCase("commit") ||  queryParts[index].equalsIgnoreCase("rollback") )) {
 						transactionQuery = transactionQuery+" "+queryParts[index];
@@ -42,6 +49,50 @@ public class TransactionProcessing {
 	
 	public static void processTransaction(String query) {
 		
+		String[] queries = query.split(";");
+		
+		//Perform the following operations only if the last transaction statement is "Commit".
+		//Because if the last statement is "Rollback", then there is no point in performing all these operations.
+		if(queries[queries.length-1].equalsIgnoreCase("commit")) {
+		for(int i=0;i<queries.length;i++) {
+			queries[i]=queries[i].trim();
+			System.out.println(queries[i]);
+			//queryLogs(queries[i])
+			String[] words = queries[i].split(" ");
+			
+			switch(words[0].trim().toLowerCase()) {
+			
+			case "select":
+				System.out.println("Calling Select");
+				//select(queries[i]);
+				break;
+				
+			case "update":
+				System.out.println("Calling Update");
+				break;
+				
+			case "create":
+				System.out.println("Calling Create Table");
+				break;
+				
+			case "delete":
+				System.out.println("Calling Delete");
+				break;
+				
+			case "drop":
+				System.out.println("Calling Drop");
+				break;
+				
+			case "insert":
+				System.out.println("Calling Insert");
+				break;
+				
+			default:
+				System.out.println("No Case Matched");
+			}
+		}
+	}
+
 	}
 
 }
